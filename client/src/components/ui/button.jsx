21b5a1915d047221f6c0a790e81ee4276ccf36d9
NAVEENKUMAR-MANI-200702 <163437596+NAVEENKUMAR-MANI-200702@users.noreply.github.com>
2026-04-16 20:06:50 +0530

    Initial setup

---
+++ client/src/components/ui/button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+export function Button({ className, children, ...props }) {
+  return (
+    <button
+      className={cn(
+        "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 bg-black text-white hover:bg-gray-800 h-10 px-4 py-2",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
---
+++ client/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    open: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
