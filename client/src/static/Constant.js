7e399a8ef6f5cb38a600a2d7fc7bf313017dfd7e
NAVEENKUMAR-MANI-200702 <163437596+NAVEENKUMAR-MANI-200702@users.noreply.github.com>
2026-04-29 17:19:12 +0530

    update

--- client/src/static/Constant.js
+++ client/src/static/Constant.js
@@ -1,5 +1,6 @@
-export let SERVER_URL = "http://localhost:9000";
+// export let SERVER_URL = "http://localhost:9000";
 // export let SERVER_URL = "http://192.168.1.46:9000";
+export let SERVER_URL = "https://record-fullstack-app.onrender.com";
 
 export const TOTAL_STEPS_LIMIT = 5;
 
